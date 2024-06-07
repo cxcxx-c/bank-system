1bf8de8172432e3ffd4a69cfc8e3ab0c47c8bdd1
cxcxx-c <dokyo0828@gmail.com>
2024-06-07 17:52:58 +0900

    개좌 개설 코드

--- src/main/java/OpenBankAccount.java
+++ src/main/java/OpenBankAccount.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class OpenBankAccount implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private final long serialVersionUID = 1L;
     private String name;
     private int accountBalance;
 
